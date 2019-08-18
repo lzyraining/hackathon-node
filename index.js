5043aff5fb8f1cb6fef7874b6076fccbcf812447
Zhuoyu Li <lzyraining@gmail.com>
2019-08-18 11:31:52 -0700

    change preferences

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const auth = require('./auth')
 const config = require('./config')
 const request = require('request')
 const path = require('path');
+const bodyParser = require('body-parser')
+
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/static', express.static(path.join(__dirname, 'public')))
 
 let token = null;
@@ -24,6 +27,7 @@ app.get('/', (req, res, next) => {
 })
 
 app.get('/lookup', (req, res, next) => {
+    const preference = req.query.preference;
     var options = {
         method: 'GET',
         url: `https://api.tealiumiq.com/v2/visitor/accounts/${config.account}/profiles/${config.profile}`,
@@ -47,9 +51,40 @@ app.get('/lookup', (req, res, next) => {
         if (error) {
             return JSON.stringify(error);
         };
-        let data = JSON.parse(body);
-        res.json(data);
+        const data = JSON.parse(body);
+        console.log(preference);
+        const top3 = [...data["visitor"]["property_sets"]["Top 3 Product Categories"]];
+        const top3WithPreferences = calculateNewTop3Categories(top3, preference, 0.45)
+        const category = {
+            'top3': top3WithPreferences,
+            'views': {...data["visitor"]["metric_sets"]["Category"]}
+        } 
+        res.json(category);
     });
 })
+
+const calculateNewTop3Categories = (categories, preference, preferenceWeight) => {
+    if (preference == null) {
+        return categories;
+    }
+    const index = categories.indexOf(preference);
+    let weight = [0.5 * (1 - preferenceWeight), 0.3 * (1 - preferenceWeight), 0.2 * (1 - preferenceWeight)]
+    if (index !== -1) {
+        weight[index] += preferenceWeight;
+    } else {
+        categories.push(preference)
+        weight.push(preferenceWeight);
+    }
+    console.log(categories);
+    console.log(weight);
+    const result = []
+    for (let i = 0; i < 3; i++) {
+        let maxIndex = weight.indexOf(Math.max(...weight));
+        result.push(categories[maxIndex]);
+        weight[maxIndex] = 0.0
+    }    
+    console.log(result)
+    return result;
+}
 
 app.listen(8080);
