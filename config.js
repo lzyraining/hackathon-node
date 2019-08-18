f5bc7e69edf717c2e3a39a58d30b77f8eb1adaf3
Zhuoyu Li <lzyraining@gmail.com>
2019-08-18 10:11:21 -0700

    add lookup api

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 module.exports = {
-    account: "tealiumlabs_hackathon",
-    profile: "sd_aug_2019_050",
+    account: "tealiumlabs-hackathon",
+    profile: "sd-aug-2019-050",
     email: "joe.zhuoyuli@gmail.com",
-    apiKey: "UzX%p6r+5DF[zpH:2mpFBw9HDpvn3QvOR)eZK:M,6b}f8yDgDg0]w9~L:4|nKN|C"
+    apiKey: "UzX%p6r+5DF[zpH:2mpFBw9HDpvn3QvOR)eZK:M,6b}f8yDgDg0]w9~L:4|nKN|C",
+    attributeId: '5088',
+    attributeValue: 'lzyraining@gmail.com'
 }
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const auth = require('./auth')
+const config = require('./config')
+const request = require('request')
 
 const app = express();
 
@@ -7,7 +9,7 @@ let token = null;
 
 app.use((req, res, next) => {
     auth.requestToken().then(token => {
-        token = token;
+        this.token = token;
         next();
     }, err => {
         console.log(err);
@@ -16,6 +18,35 @@ app.use((req, res, next) => {
 
 app.get('/', (req, res, next) => {
     res.json({"status": "success"})
+})
+
+app.get('/lookup', (req, res, next) => {
+    var options = {
+        method: 'GET',
+        url: `https://api.tealiumiq.com/v2/visitor/accounts/${config.account}/profiles/${config.profile}`,
+        qs:
+        {
+            attributeId: config.attributeId,
+            attributeValue: config.attributeValue,
+            prettyName: 'true'
+        },
+        headers:
+        {
+            'Postman-Token': 'bfe41502-1949-4243-b591-bfac90817922',
+            'cache-control': 'no-cache',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            Authorization: `Bearer ${this.token}`
+        },
+        form: { undefined: undefined }
+    };
+
+    request(options, function (error, response, body) {
+        if (error) {
+            return JSON.stringify(error);
+        };
+        let data = JSON.parse(body);
+        res.json(data);
+    });
 })
 
 app.listen(8080);
